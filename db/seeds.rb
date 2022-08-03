4253debeafffb1a77292b2dbbb12232c11260584
Alicia <arrowsrp@yahoo.com>
2022-08-03 14:31:27 -0700

    Adds seed function for Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
+require 'faker'
 
 puts "🌱 Seeding spices..."
+
+10.times do
+    User.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email
+    )
+end
 
 # Seed your database here
 
